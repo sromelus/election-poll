ea15ce47ce825da3560dea69c99da026cb302ba9
Shardly Romelus <rshardlypicard89@gmail.com>
2024-10-28 17:10:35 -0400

    added react-model library

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ function App() {
       <Header />
       <VoteSelection />
       <StyledFooter>
-        © 2024 website.org
+        © 2024 unofficalpoll.com
       </StyledFooter>
     </AppContainer>
   );
