7dab590a7d1fc7b58fbcbe9b6c438a8c2eeea793
Shardly Romelus <rshardlypicard89@gmail.com>
2024-10-29 11:38:07 -0400

    binded react-modal to #root element and fixed indentations

--- src/components/voteSelectionContainer/VoteSelection.tsx
+++ src/components/voteSelectionContainer/VoteSelection.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import CandidateCard from './CandidateCard';
 import ProgressBar from './ProgressBar';
 import styled from 'styled-components';
@@ -22,11 +22,38 @@ const ProgressBarContainer = styled.div`
 `;
 
 const VoteSelection = () => {
-    const [trumpPollNumbers, setTrumpPollNumbers] = useState<number>(13);
-    const [kamalaPollNumbers, setKamalaPollNumbers] = useState<number>(34);
+    const [trumpPollNumbers, setTrumpPollNumbers] = useState<number>(0);
+    const [kamalaPollNumbers, setKamalaPollNumbers] = useState<number>(0);
     const [gender, setGender] = useState<string>('');
     const [ethnicity, setEthnicity] = useState<string>('');
     const [maxPercentage, setMaxPercentage] = useState<number>(100)
+
+    useEffect(() => {
+        getVotes()
+    }, [trumpPollNumbers, kamalaPollNumbers]);
+
+    const getVotes = () => {
+        fetch('http://localhost:8080/api/votes')
+            .then(response => response.json())
+            .then(data => {
+                setTrumpPollNumbers(data.trump)
+                setKamalaPollNumbers(data.kamala)
+            })
+            .catch(error => {
+                console.error('Error fetching vote selection data:', error);
+            });
+    }
+
+    const postVote = (candidate: string) => {
+        const data = { candidate: candidate }
+        fetch('http://localhost:8080/api/votes', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data)
+        })
+    }
 
     const updateMaxPercentageReached = () => {
         const highestPollPercentage = Math.max(trumpPollNumbers, kamalaPollNumbers)
@@ -39,6 +66,7 @@ const VoteSelection = () => {
     const handleTrumpVote = () => {
         setTrumpPollNumbers(trumpPollNumbers + 1)
         updateMaxPercentageReached()
+        postVote('trump')
         setGender('')
         setEthnicity('')
     }
@@ -46,52 +74,52 @@ const VoteSelection = () => {
     const handleKamalaVote = () => {
         setKamalaPollNumbers(kamalaPollNumbers + 1)
         updateMaxPercentageReached()
+        postVote('kamala')
         setGender('')
         setEthnicity('')
     }
 
+    return (
+        <>
+            <CandidateCardContainer>
+                <CandidateCard
+                    label="Trump"
+                    button_color="red"
+                    image={trumpImage}
+                    onVote={handleTrumpVote}
+                    gender={gender}
+                    setGender={setGender}
+                    ethnicity={ethnicity}
+                    setEthnicity={setEthnicity}
+                />
+                <CandidateCard
+                    label="Kamala"
+                    button_color="blue"
+                    image={kamalaImage}
+                    onVote={handleKamalaVote}
+                    gender={gender}
+                    setGender={setGender}
+                    ethnicity={ethnicity}
+                    setEthnicity={setEthnicity}
+                />
+            </CandidateCardContainer>
 
-  return (
-    <>
-        <CandidateCardContainer>
-            <CandidateCard
-                label="Trump"
-                button_color="red"
-                image={trumpImage}
-                onVote={handleTrumpVote}
-                gender={gender}
-                setGender={setGender}
-                ethnicity={ethnicity}
-                setEthnicity={setEthnicity}
-            />
-            <CandidateCard
-                label="Kamala"
-                button_color="blue"
-                image={kamalaImage}
-                onVote={handleKamalaVote}
-                gender={gender}
-                setGender={setGender}
-                ethnicity={ethnicity}
-                setEthnicity={setEthnicity}
-            />
-        </CandidateCardContainer>
+            <ProgressBarContainer>
+                <div>
+                    <p style={{fontSize: '1.2rem', fontWeight: 'bold'}}>Trump {trumpPollNumbers}</p>
+                    <ProgressBar color="red" percentage={trumpPollNumbers} maxPercentage={maxPercentage} />
+                </div>
+                <div>
+                    <p style={{fontSize: '1.2rem', fontWeight: 'bold'}}>Kamala {kamalaPollNumbers}</p>
+                    <ProgressBar color="blue" percentage={kamalaPollNumbers} maxPercentage={maxPercentage} />
+                </div>
+            </ProgressBarContainer>
 
-        <ProgressBarContainer>
             <div>
-                <p style={{fontSize: '1.2rem', fontWeight: 'bold'}}>Trump {trumpPollNumbers}</p>
-                <ProgressBar color="red" percentage={trumpPollNumbers} maxPercentage={maxPercentage} />
+                <p>You are a {ethnicity.toLowerCase()} {gender.toLowerCase()} showing for Trump {trumpPollNumbers} votes, Kamala {kamalaPollNumbers} votes</p>
             </div>
-            <div>
-                <p style={{fontSize: '1.2rem', fontWeight: 'bold'}}>Kamala {kamalaPollNumbers}</p>
-                <ProgressBar color="blue" percentage={kamalaPollNumbers} maxPercentage={maxPercentage} />
-            </div>
-        </ProgressBarContainer>
-
-        {gender && ethnicity && (
-            <p>You are a {ethnicity.toLowerCase()} {gender.toLowerCase()} showing for Trump {trumpPollNumbers} votes, Kamala {kamalaPollNumbers} votes</p>
-        )}
-    </>
-  );
+        </>
+    );
 };
 
 export default VoteSelection;
